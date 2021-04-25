441ae2ad1e08f600cc038744b764b4e96308faed
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-04-25 20:07:36 -0300

    realizar teste utilizando classe de mock

---
+++ tests/Service/EncerradorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Service;
+
+use App\Dao\Leilao as LeilaoDao;
+use App\Model\Leilao;
+use App\Service\Encerrador;
+use PHPUnit\Framework\TestCase;
+
+
+class LeilaoDaoMock extends LeilaoDao
+{
+    private $leiloes = [];
+
+    public function salva(Leilao $leilao): void
+    {
+        $this->leiloes[] = $leilao;
+    }
+
+    public function recuperarFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+            return $leilao->estaFinalizado();
+        });
+    }
+
+    public function recuperarNaoFinalizados(): array
+    {
+        return array_filter($this->leiloes, function (Leilao $leilao) {
+            return !$leilao->estaFinalizado();
+        });
+    }
+
+    public function atualiza(Leilao $leilao)
+    {
+        return;
+    }
+}
+
+class EncerradorTest extends TestCase
+{
+    public function testDeveEncerrarLeiloesComMaisDeUmaSemana()
+    {
+        $leilaoFiat = new Leilao('Fiat 147 0Km', new \DateTimeImmutable('8 days ago'));
+        $leilaoVariante = new Leilao('Variante 0Km', new \DateTimeImmutable('10 days ago'));
+
+        $leilaoDao = new LeilaoDaoMock();
+        $leilaoDao->salva($leilaoFiat);
+        $leilaoDao->salva($leilaoVariante);
+
+        $encerrador = new Encerrador($leilaoDao);
+        $encerrador->encerra();
+
+        $leiloesEncerrados = $leilaoDao->recuperarFinalizados();
+        static::assertCount(2, $leiloesEncerrados);
+        static::assertEquals(
+            'Fiat 147 0Km',
+            $leiloesEncerrados[0]->recuperarDescricao()
+        );
+        static::assertEquals(
+            'Variante 0Km',
+            $leiloesEncerrados[1]->recuperarDescricao()
+        );
+    }
+}
