fc77570204b13b58c4825b2341f4fad58df77cdf
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-05-23 15:45:49 -0300

    utilizando dataProvider

--- tests/Integration/Dao/LeilaoDaoTest.php
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Integration\Dao;
 
 use App\Dao\Leilao as DaoLeilao;
-use App\Infra\ConnectionCreator;
 use App\Model\Leilao;
 use PHPUnit\Framework\TestCase;
 
@@ -28,11 +27,16 @@ class LeilaoDaoTest extends TestCase
         self::$pdo->beginTransaction();
     }
 
-    public function testInsercaoEBuscaDevemFuncionar()
+    
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesNaoFinalizados(array $leiloes)
     {
-        $leilao = new Leilao('Variante 0Km');
         $leilaoDao = new DaoLeilao(self::$pdo);
-        $leilaoDao->salva($leilao);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
 
         $leiloes = $leilaoDao->recuperarNaoFinalizados();
     
@@ -42,10 +46,47 @@ class LeilaoDaoTest extends TestCase
             'Variante 0Km',
             $leiloes[0]->recuperarDescricao()
         );
+        self::assertFalse($leiloes[0]->estaFinalizado());
     }
+
+    /**
+     * @dataProvider leiloes
+     */
+    public function testBuscaLeiloesFinalizados(array $leiloes)
+    {
+        $leilaoDao = new DaoLeilao(self::$pdo);
+        foreach ($leiloes as $leilao) {
+            $leilaoDao->salva($leilao);
+        }
+
+        $leiloes = $leilaoDao->recuperarFinalizados();
+
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame(
+            'Fiat 147 0Km',
+            $leiloes[0]->recuperarDescricao()
+        );
+        self::assertTrue($leiloes[0]->estaFinalizado());
+    }
+
 
     protected function tearDown(): void
     {
         self::$pdo->rollBack();
     }
+
+    public function leiloes()
+    {
+        $naoFinalizado = new Leilao('Variante 0Km');
+        $finalizado = new Leilao('Fiat 147 0Km');
+        $finalizado->finaliza();
+
+        return [
+            [
+                [$naoFinalizado, $finalizado]
+            ]
+        ];
+    }
 }
+
